3c0879eafb8c1d5fb36b0f4abd834fa7d9fd2b3d
c-dez <c-dez@proton.me>
2024-10-18 00:21:25 -0600

    added inputs to player_movement.gd

--- scenes/player/player_movement.gd
+++ scenes/player/player_movement.gd
@@ -3,20 +3,23 @@ extends CharacterBody3D
 
 @export var speed:float = 5.0
 @export var jump_velocity:float = 4.5
+var gravity:Vector3
 
+func _ready() -> void:
+	gravity = get_gravity()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir : Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir : Vector2 = Input.get_vector("left", "right", "forward", "backwards")
 	var direction :Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * speed

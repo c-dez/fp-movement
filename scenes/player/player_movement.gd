4d9286ebba8c198aa9a74bab7f33be6020f27ae6
c-dez <c-dez@proton.me>
2024-10-29 20:51:31 -0600

    anadido dash

--- scenes/player/player_movement.gd
+++ scenes/player/player_movement.gd
@@ -2,57 +2,76 @@ extends CharacterBody3D
 
 
 @onready var player_mesh:Node3D = get_node("MeshInstance3D")
-@onready var dash_timer:Timer = get_node("DashTimer")
 
-@export var speed:float = 7.0
-@export var jump_velocity:float = 7.5
-@export var rotate_mesh_weight:float = 0.2
-@export var dash_countdown_time:float = 1.0
+# Move
+var speed:float = 7.0
+var jump_velocity:float = 7.5
+# DASH
+var dash_speed:float = 14
+var dash_countdown_time:float = 0.3
 # INTERNAL
 var dash_countdown_time_internal:float = 0
+# MISC
+var rotate_mesh_weight:float = 0.2
 
+enum MOVE_STATE {NORMAL, DASH}
+# move_state usa enum MOVE_STATE para diferenciar entre moverse a velocidad normal, dash
+var move_state:int
 
 
 func _ready() -> void:
-	# dash_timer.one_shot = true
-	# dash_timer.timeout.connect(_on_dash_player_timeout)
+	move_state = MOVE_STATE.NORMAL
 	pass
 
+func _process(delta: float) -> void:
+	change_move_state_to_dash_countdown(delta)
+	# QUIERO QUE LA VELOCIDAD DE DASH SE MANTENGA EN EL AIRE AUNQUE YA HALLA EXPIRADO LA CUENTA REGRESIVA DE  change_move_state_to_dash_countdown QUEDA PENDIENTE
+
+	
+	pass
+
+
 func _physics_process(delta: float) -> void:
-	move_player()
+	move_player_speed_match()
 	jump_player()
 	gravity_player(delta)
 	move_and_slide()
-	dash_player(delta)
-
 	pass
 
 
-func dash_player(_delta:float):
-	# esta funcion se encarga de ejecutar codigo durante x segundos
-
-	# al presionar shift, el valor de dash_coutdown_time se asigna a el internal 
+func change_move_state_to_dash_countdown(_delta:float)->void:
+	# esta funcion se encarga de cambiar valor de move_state entre normal y dashing al presionar shift, durante x tiempo move_state == 1, (enum MOVE_STATE.DASH)
 	if Input.is_action_just_pressed("shift"):
 		dash_countdown_time_internal = dash_countdown_time
-	# mientras internal sea mayor que cero, se ejecuta bloque
-	while dash_countdown_time_internal > 0:
-		# CODIGO
-		print(dash_countdown_time_internal)
-		# ROMPE EL LOOP
+	if dash_countdown_time_internal > 0 :
 		dash_countdown_time_internal -= _delta
+		move_state = MOVE_STATE.DASH
+	elif dash_countdown_time_internal < 0:
+		dash_countdown_time_internal = 0
+		move_state = MOVE_STATE.NORMAL
+		pass
+	pass
+
+func move_player_speed_match():
+	# elige la velocidad de movimiento segun el estado
+	match move_state:
+		MOVE_STATE.NORMAL:
+			move_player(speed)
+		MOVE_STATE.DASH:
+			move_player(dash_speed)
 	pass
 
 
-func move_player()->void:
-	# Get the input direction and handle the movement/deceleration.
+func move_player(_speed:float)->void:
+	# mueve a el Player segun _speed
 	var input_dir : Vector2 = Input.get_vector("left", "right", "forward", "backwards")
 	var direction :Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * speed
-		velocity.z = direction.z * speed
+		velocity.x = direction.x * _speed 
+		velocity.z = direction.z * _speed 
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		velocity.z = move_toward(velocity.z, 0, speed)
+		velocity.x = move_toward(velocity.x, 0, _speed)
+		velocity.z = move_toward(velocity.z, 0, _speed)
 	rotate_mesh(input_dir)	
 
 
